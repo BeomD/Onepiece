553d92abe42cb3fd2ccde96719e6869015468486
Beom.D <bamjin204@gmail.com>
2015-09-30 15:37:22 +0900

    modified:   apps/player.c 	modified:   lib/func1.c 	modified:   lib/func2.c

--- apps/player.c
+++ apps/player.c
@@ -2,5 +2,8 @@ int main(void)
 {
   func1();
   func2();
+  //수정 부분
+  func1();
+  func2();
 
 }
--- lib/func1.c
+++ lib/func1.c
@@ -2,5 +2,6 @@
 
 void func1()
 {
-printf("It's func1 룰루 룰루!\n");
+printf("It's func1 호호!\n");
+printf("이게있으면 수정된거두번이나 수정된거.");
 }
